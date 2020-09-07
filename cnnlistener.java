db438b5e47ddbe1bd69b99e5e9e5c0b0e37d00de
Shoaib Vijapure <shoaibvijapure@gmail.com>
2020-09-07 22:25:45 +0530

    Add files via upload

---
+++ cnnlistener.java
@@ -0,0 +1,6 @@
+//*****CNNLISTENER.java
+package com.sh1r0.caffe_android_demo;
+
+public interface CNNListener {
+    void onTaskCompleted(int result);
+}
---
+++ main2activity.java
@@ -0,0 +1,36 @@
+//******** main2activity.java
+
+package com.sh1r0.caffe_android_demo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class Main2Activity extends Activity {
+    private Button btnSelect,btweb;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        btnSelect = (Button) findViewById(R.id.button2);
+        btweb = (Button) findViewById(R.id.button3);
+        btnSelect.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+              Intent i=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        });
+        btweb.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent i=new Intent(getApplicationContext(),WebViewAct.class);
+                startActivity(i);
+            }
+        });
+    }
+}
